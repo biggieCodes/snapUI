2ba7526d81bf2a0e154025e5410057747ce923b6
biggiecodes <raboabdulsamad04@gmail.com>
2022-08-08 16:00:28 +0100

    first commit

---
+++ src/components/Body/Body.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Body.css'
+function Body() {
+  return (
+    <div className='body1'>
+        <h1>Make<br />remote work</h1>
+
+       <div className='container'>
+        <p>Get your team in sync, no matter your location.
+            Streamline processes,create team rituals and watch productivity soar.
+        </p>
+       </div>
+        <button> Learn More</button>
+    </div>
+  )
+}
+
+export default Body
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar 
+() {
+  return (
+    <div className='nav'>
+         <ul className='parentlist'>
+            <div className='li1'><li> Snap </li></div>
+                <li>Features</li>
+                <li>Company</li>
+                <li>Careers</li>
+                <li>About</li>
+            </ul>
+    </div>
+  )
+}
+
+export default Navbar
+
